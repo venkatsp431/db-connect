84f9fe7c9df07973f8853aa30d48a2866bd87a3a
venkatsp431 <sealvenki@gmail.com>
2023-06-13 17:20:39 +0530

    final commit

--- Routers/users.js
+++ Routers/users.js
@@ -38,7 +38,7 @@ router.post("/login", async (req, res) => {
       },
     });
   } catch (error) {
-    res.status(500).json({ data: "InternalServer Eroor" });
+    res.status(500).json({ data: "InternalServer Eroor", error: error });
   }
 });
 
